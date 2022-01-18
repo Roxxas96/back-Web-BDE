bc5f46fa56bde888b3f071adb01337f12bbfa3be
Roxxas96 <gomez.a.corneille@gmail.com>
2022-01-18 22:36:05 +0100

    added default values on db init script

--- database/initdb.sql
+++ database/initdb.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "Users" (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50),
-    surname VARCHAR(50),
+    name VARCHAR(50) DEFAULT '',
+    surname VARCHAR(50) DEFAULT '',
     pseudo VARCHAR(50) DEFAULT 'Anonym user',
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(200) NOT NULL CHECK (char_length(password) > 7),
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS "Sessions" (
 CREATE TABLE IF NOT EXISTS "Challenges" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) DEFAULT 'Unnamed challenge',
-    description TEXT,
+    description TEXT DEFAULT '',
     reward INTEGER DEFAULT 0 CHECK (reward >= 0),
     "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     "creatorId" INTEGER NOT NULL,
