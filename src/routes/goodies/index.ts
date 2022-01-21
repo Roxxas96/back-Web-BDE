2d525081db89d8a31d87606450e4d5a5c4461ff3
Roxxas96 <gomez.a.corneille@gmail.com>
2022-01-21 19:47:10 +0100

    Goodies Route

--- src/routes/goodies/index.ts
+++ src/routes/goodies/index.ts
@@ -1,6 +1,10 @@
 import { Goodies } from "@prisma/client";
 import { FastifyPluginAsync } from "fastify";
-import { GoodiesInfo, GoodiesInfoMinimal } from "../../models/GoodiesInfo";
+import {
+  GoodiesInfo,
+  GoodiesInfoMinimal,
+  GoodiesSchema,
+} from "../../models/GoodiesInfo";
 import {
   createGoodies,
   deleteGoodies,
@@ -15,6 +19,12 @@ const goodiesRoute: FastifyPluginAsync = async (
 ): Promise<void> => {
   fastify.get<{ Reply: GoodiesInfoMinimal[] }>(
     "/",
+    {
+      schema: {
+        tags: ["goodies"],
+        description: "Fetch all goodies",
+      },
+    },
     async function (request, reply) {
       await fastify.auth.authenticate(request.headers);
 
@@ -26,6 +36,20 @@ const goodiesRoute: FastifyPluginAsync = async (
 
   fastify.get<{ Params: { id: string }; Reply: Goodies }>(
     "/:id",
+    {
+      schema: {
+        tags: ["goodies"],
+        description: "Get a specific goodies info",
+        params: {
+          type: "object",
+          description: "Id of the goodies to fetch",
+          properties: {
+            id: { type: "number" },
+          },
+          required: ["id"],
+        },
+      },
+    },
     async function (request, reply) {
       await fastify.auth.authenticate(request.headers);
 
@@ -37,6 +61,13 @@ const goodiesRoute: FastifyPluginAsync = async (
 
   fastify.put<{ Body: GoodiesInfo; Reply: string }>(
     "/",
+    {
+      schema: {
+        tags: ["goodies", "admin"],
+        description: "Create a goodies with provided info",
+        body: GoodiesSchema,
+      },
+    },
     async function (request, reply) {
       const userId = await fastify.auth.authenticate(request.headers);
 
@@ -52,6 +83,21 @@ const goodiesRoute: FastifyPluginAsync = async (
 
   fastify.patch<{ Body: GoodiesInfo; Params: { id: string }; Reply: string }>(
     "/:id",
+    {
+      schema: {
+        tags: ["goodies", "admin"],
+        description: "Update a goodies with the provided info",
+        body: GoodiesSchema,
+        params: {
+          type: "object",
+          description: "Id of the goodies to update",
+          properties: {
+            id: { type: "number" },
+          },
+          required: ["id"],
+        },
+      },
+    },
     async function (request, reply) {
       const userId = await fastify.auth.authenticate(request.headers);
 
@@ -67,6 +113,20 @@ const goodiesRoute: FastifyPluginAsync = async (
 
   fastify.delete<{ Params: { id: string } }>(
     "/:id",
+    {
+      schema: {
+        tags: ["goodies", "admin"],
+        description: "Delete a specific goodies",
+        params: {
+          type: "object",
+          description: "Id of the goodies to delte",
+          properties: {
+            id: { type: "number" },
+          },
+          required: ["id"],
+        },
+      },
+    },
     async function (request, reply) {
       const userId = await fastify.auth.authenticate(request.headers);
 
