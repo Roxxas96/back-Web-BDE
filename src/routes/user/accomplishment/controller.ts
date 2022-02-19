830e87e367b14a4fbe6e5770a78c2982ca6f3195
Roxxas96 <gomez.a.corneille@gmail.com>
2022-02-19 16:13:35 +0100

    added accomplishment related routes on /user route

--- src/routes/user/accomplishment/controller.ts
+++ src/routes/user/accomplishment/controller.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from "fastify";
+import { AccomplishmentInfo } from "../../../models/AccomplishmentInfo";
 
+//Get user's accomplishments
 export async function getUserAccomplishment(
   fastify: FastifyInstance,
   userId: number
@@ -7,9 +9,56 @@ export async function getUserAccomplishment(
   const accomplishments =
     await fastify.prisma.accomplishment.getManyAccomplishment(userId);
 
+  //Check for empty result
   if (!accomplishments || !accomplishments.length) {
     throw fastify.httpErrors.notFound("No Accomplishment found");
   }
 
   return accomplishments;
+}
+
+//Create an accomplishment for the current user
+export async function createAccomplishment(
+  fastify: FastifyInstance,
+  accomplishmentInfo: AccomplishmentInfo,
+  userId: number,
+  challengeId: number
+) {
+  //Check for userId
+  if (!userId) {
+    throw fastify.httpErrors.badRequest("Invalid user id");
+  }
+
+  //Check for challengeId
+  if (!challengeId) {
+    throw fastify.httpErrors.badRequest("Invalid challenge id");
+  }
+
+  //Check for infos
+  if (!accomplishmentInfo) {
+    throw fastify.httpErrors.badRequest("No accomplishment info provided");
+  }
+
+  const ownedAccomplishments =
+    await fastify.prisma.accomplishment.getManyAccomplishment(userId);
+
+  if (
+    ownedAccomplishments.filter((accomplishment) => {
+      return (
+        (accomplishment.validation === 1 ||
+          accomplishment.validation === null) &&
+        accomplishment.challengeId === challengeId
+      );
+    }).length
+  ) {
+    throw fastify.httpErrors.badRequest(
+      "You already have a Pending or an Accepted accomplishment"
+    );
+  }
+
+  await fastify.prisma.accomplishment.createAccomplishment(
+    accomplishmentInfo,
+    userId,
+    challengeId
+  );
 }
--- src/routes/user/accomplishment/index.ts
+++ src/routes/user/accomplishment/index.ts
@@ -1,6 +1,10 @@
 import { Accomplishment } from "@prisma/client";
 import { FastifyPluginAsync } from "fastify";
-import { getUserAccomplishment } from "./controller";
+import {
+  AccomplishmentInfo,
+  AccomplishmentSchema,
+} from "../../../models/AccomplishmentInfo";
+import { createAccomplishment, getUserAccomplishment } from "./controller";
 
 const userAccomplishmentRoute: FastifyPluginAsync = async (
   fastify,
@@ -9,17 +13,79 @@ const userAccomplishmentRoute: FastifyPluginAsync = async (
   fastify.get<{
     Params: { id: number };
     Reply: { message: string; accomplishments: Accomplishment[] };
-  }>("/:id", async function (request, reply) {
-    const userId = await fastify.auth.authenticate(request.headers);
+  }>(
+    "/:id",
+    {
+      schema: {
+        tags: ["user", "accomplishment"],
+        description: "Get a user accomplishments",
+        params: {
+          type: "object",
+          description: "Id of the user",
+          properties: {
+            id: { type: "number" },
+          },
+          required: ["id"],
+        },
+      },
+    },
+    async function (request, reply) {
+      const userId = await fastify.auth.authenticate(request.headers);
 
-    if (request.params.id !== userId) {
-      await fastify.auth.authorize(userId, 1);
+      if (request.params.id !== userId) {
+        await fastify.auth.authorize(userId, 1);
+      }
+
+      const accomplishments = await getUserAccomplishment(fastify, request.id);
+
+      return reply.status(200).send({ message: "Success", accomplishments });
     }
+  );
 
-    const accomplishments = await getUserAccomplishment(fastify, request.id);
+  fastify.put<{
+    Body: { info: AccomplishmentInfo; challengeId: number };
+    Reply: { message: string };
+  }>(
+    "/:id",
+    {
+      schema: {
+        tags: ["accomplishment"],
+        description:
+          "Create an accomplishment for a user with the provided info",
+        body: {
+          type: "object",
+          properties: {
+            info: AccomplishmentSchema,
+            challengeId: {
+              type: "number",
+              description: "Id of the challenge related to the accomplishment",
+            },
+          },
+          required: ["info", "challengeId"],
+        },
+        params: {
+          type: "object",
+          description: "Id of the user",
+          properties: {
+            id: { type: "number" },
+          },
+          required: ["id"],
+        },
+      },
+    },
+    async function (request, reply) {
+      const userId = await fastify.auth.authenticate(request.headers);
 
-    return reply.status(200).send({ message: "Success", accomplishments });
-  });
+      await createAccomplishment(
+        fastify,
+        request.body.info,
+        userId,
+        request.body.challengeId
+      );
+
+      return reply.status(201).send({ message: "Success" });
+    }
+  );
 };
 
 export default userAccomplishmentRoute;
