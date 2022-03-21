427216f5ce27049790af830faeeaa336568146fd
Roxxas96 <gomez.a.corneille@gmail.com>
2022-03-21 23:43:31 +0100

    fix: user already exists on user creation

--- src/routes/user/controller.ts
+++ src/routes/user/controller.ts
@@ -36,6 +36,8 @@ export async function modifyUserInfo(
 
   const user = await fastify.prisma.user.getUser(userId);
 
+  console.log(user);
+
   if (!user) {
     throw fastify.httpErrors.notFound("User not found");
   }
@@ -84,6 +86,14 @@ export async function createUser(
   //Check password length
   if (userInfo.password.length < 8) {
     throw fastify.httpErrors.badRequest("User password is too small");
+  }
+
+  const user = await fastify.prisma.user.getUser(undefined, userInfo.email);
+
+  if (user) {
+    throw fastify.httpErrors.badRequest(
+      "A user already exists with this email"
+    );
   }
 
   //Hash password
